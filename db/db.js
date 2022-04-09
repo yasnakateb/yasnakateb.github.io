d3b660288f5042a818e3883a2dcb715f43616d06
yasnakateb <yasnakatebzadeh@gmail.com>
2022-04-09 19:36:07 +0430

    Update

---
+++ db/db.js
@@ -0,0 +1,230 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"",
+			],
+		},
+		contact: {
+			text: [
+				"",
+			],
+		},
+	},
+	skills: [
+		
+		
+	],
+	projects: {
+		web: [
+			{
+				projectName: "Pipelined ARM",
+				image: "",
+				summary:
+					"A 32-bit ARM Processor Implementation in Verilog HDL",
+				preview: "https://github.com/yasnakateb/PipelinedARM",
+				techStack: [
+					
+				],
+			},
+			{
+				projectName: "Router",
+				image: "",
+				summary:
+					"Design of a router with components like FIFO, register, synchronizer and FSM",
+				preview: "https://github.com/yasnakateb/Router",
+				techStack: [],
+			},
+			{
+				projectName: "Pipelined MIPS",
+				image: "",
+				summary:
+					"A 16-bit mips processor implementation in Verilog HDL",
+				preview: "https://github.com/yasnakateb/PipelinedMIPS",
+				techStack: [],
+			},
+			{
+				projectName: "Blinky",
+				image: "",
+				summary:
+					"A Quartus II project testing the functionality of the Altera Cyclone IV EP4CE6E22C8N board ",
+				preview: "https://github.com/yasnakateb/Blinky",
+				techStack: [],
+			},
+
+			{
+				projectName: "Advanced Encryption Standard",
+				image: "",
+				summary:
+					"Hardware implementation of AES algorithm in Verilog HDL",
+				preview: "https://github.com/yasnakateb/AES",
+				techStack: [],
+			},
+			{
+				projectName: "UART",
+				image: "",
+				summary:
+					"UART Communication Implementation in Verilog HDL",
+				preview: "https://github.com/yasnakateb/UARTCommunication",
+				techStack: [],
+			},
+			{
+				projectName: "SDRAM controller",
+				image: "",
+				summary:
+					"A SDRAM controller in Verilog HDL ",
+				preview: "https://github.com/yasnakateb/SdramController",
+				techStack: [],
+			},
+			{
+				projectName: "Traffic Light Controller",
+				image: "",
+				summary:
+					"A digital controller to control traffic in Verilog HDL",
+				preview: "https://github.com/yasnakateb/TrafficLightController",
+				techStack: [],
+			},
+			{
+				projectName: "FIFO Memory",
+				image: "",
+				summary:
+					"A FIFO memory implementation in Verilog HDL",
+				preview: "https://github.com/yasnakateb/FIFOMemory",
+				techStack: [],
+			},
+
+
+
+
+		],
+		software: [
+			{
+				projectName: "Chatroom",
+				image: "",
+				summary:
+					"A simple chatroom (server-client) in Rust",
+				preview:
+					"https://github.com/yasnakateb/Chatroom",
+				techStack: ["Rust"],
+			},
+			{
+				projectName: "SingleProcessorSystem",
+				image: "",
+				summary:
+					"A Single Processor System With Gem5 ",
+				preview: "https://github.com/yasnakateb/SingleProcessorSystem",
+				techStack: ["C" , "Python"],
+			},
+			{
+				projectName: "Dartris",
+				image: "",
+				summary:
+					"A simple Tetris Game in Dart",
+				preview: "https://github.com/yasnakateb/Dartris",
+				techStack: ["Dart"],
+			},
+			{
+				projectName: "TinyOS ",
+				image: "",
+				summary:
+					"A simple Operating System in Nasm Assembly and C ",
+				preview: "https://github.com/yasnakateb/TinyOS",
+				techStack: ["C", "NASM"],
+			},
+			
+		],
+		image: [
+			{
+				projectName: "Image segmentation system",
+				image: "",
+				summary:
+					"Threshold operation implementation in Verilog HDL",
+				preview: "https://github.com/yasnakateb/Threshold",
+				techStack: [],
+			},
+			{
+				projectName: "MNIST Neural Network",
+				image: "",
+				summary:
+					"Simple neural network using the MNIST data set to recognize hand-written digits.",
+				preview: "https://github.com/yasnakateb/MNISTNeuralNetwork",
+				techStack: [],
+			},
+
+		]
+		
+	},
+	
+	education: [
+		{
+			title: "Bachelors in Computer Science and Engineering",
+			duration: "",
+			subtitle: "Shiraz University",
+			details: [
+				
+
+
+				"Since 2017: Shiraz University, Shiraz, Iran",
+				"Supervisor: Dr. Mohsen Raji",
+				
+			],
+			tags: [
+				"Computer Architecture",
+				"Operating Systems",
+				"Database Management System",
+				"FPGAs",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "National Organization for Development of Exceptional Talents ",
+			duration: "",
+			subtitle: "Farzanegan School",
+			details: [
+				"2013-2017: Farzanegan School, Shiraz, Iran",
+				
+			],
+			tags: ["Physics", "Mathematics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				
+				{
+					text: "GitHub",
+					link: "https://github.com/yasnakateb",
+				},
+				
+			],
+		},
+		
+		
+		
+
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/yasna-katebzadeh-71a8a0202/",
+				},
+				
+			],
+		}, 
+		{
+			label: "Email",
+			data: [
+				{
+					text: "yasna[dot]katebzadeh[at]gmail[dot]com"
+					
+					
+				},
+				
+				
+			],
+		}
+	],
+};
